e3266ef2a240c8792c664d67e672d6b5e4efa9f3
Marlon Henry Schweigert <fleyhe0@gmail.com>
2017-09-20 15:44:16 -0300

    Atom Setup

--- startup.sh
+++ startup.sh
@@ -1,2 +1,19 @@
+apt-get update
+apt-get upgrade
+
+wget https://atom-installer.github.com/v1.20.1/atom-amd64.deb
+wget https://release.gitkraken.com/linux/gitkraken-amd64.deb
+
+dpkg -i gitkraken-amd64.deb -y
+dpkg -i atom.amd64.deb -y
+
+apt-get install -f
+
+rm *.deb
+
+apt-get install rvm
+source $HOME/.rvm/scripts/rvm
+
+
 apm install atom-beautify
 apm install minimap
