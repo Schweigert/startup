842ff62a80210f97b79c080456501a687fb9d8dd
Marlon Henry Schweigert <marlon.henry@magrathealabs.com>
2017-09-22 14:32:32 -0300

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -7,11 +7,13 @@ wget https://release.gitkraken.com/linux/gitkraken-amd64.deb
 dpkg -i gitkraken-amd64.deb -y
 dpkg -i atom.amd64.deb -y
 
+apt-get install curl
 apt-get install -f
 
 rm *.deb
 
-apt-get install rvm
+\curl -sSL https://get.rvm.io | bash -s stable
+echo 'source /usr/local/rvm/scripts/rvm' >> ~/.bashrc
 source $HOME/.rvm/scripts/rvm
 
 
